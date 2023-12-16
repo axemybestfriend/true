ba36aeee7e84ea2b21d0e3ff0d6ceb6819249cab
axemybestfriend <axemybestfriend@mail.ru>
2023-12-16 16:05:24 +0700

    Update Product.java
    try,catch,throw

--- Product.java
+++ Product.java
@@ -20,7 +20,25 @@ public class Product
     int getid() {
         return this.id;
     }
- 
+    void changeprice() {
+       try{ 
+           Scanner scn = new Scanner(System.in);
+           System.out.print("Введите новую цену товара : ");
+           int a = scn.nextInt();
+           if (a < 0){
+               throw new Exception("Новая цена вне диапозона реальных цен");
+            }
+           if (a == this.price){
+               throw new Exception("Новая цена совпадает со старой,изменения не будут произведены");
+           }
+           this.price = a;
+           scn.close();
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+       
+    }
     void changeid() {
         Scanner scn = new Scanner(System.in);
         System.out.print("Введите новый id товара : ");
